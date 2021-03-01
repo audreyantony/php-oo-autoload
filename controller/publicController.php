53cbca0088b346ea129a6412222ea2ee8bcb6f4f
mikhawa <michaeljpitz@gmail.com>
2021-03-01 09:38:58 +0100

    getAllWithoutTheSectionDesc()

--- controller/publicController.php
+++ controller/publicController.php
@@ -1,7 +1,7 @@
 <?php
 
 // récupération des sections pour le menu
-$sectionsForMenu = $TheSectionManager->getAll();
+$sectionsForMenu = $TheSectionManager->getAllWithoutTheSectionDesc();
 
 // Appel de la vue (objet de type Twig, la méthode render utilise un modèle Twig se trouvant dans view, suivi de paramètres)
-echo $twig->render("indexPublic.html.twig",["menu"=>$sectionsForMenu]);
+echo $twig->render("publicView/index_public.html.twig",["menu"=>$sectionsForMenu]);
--- model/TheSectionManager.php
+++ model/TheSectionManager.php
@@ -4,10 +4,26 @@
 class TheSectionManager extends ManagerAbstract implements ManagerInterface
 {
 
-    // récupération de toutes les sections
+    // récupération de tous les champs de toutes les sections
     public function getAll(): array
     {
         $sql="SELECT * FROM TheSection";
+        $recup = $this->db->query($sql);
+        if(!$recup->rowCount()){
+            return [];
+        }
+        $array = $recup->fetchAll(PDO::FETCH_ASSOC);
+        // instanciations des résultats en objets de type TheSection
+        foreach ($array as $item){
+            $sections[]= new TheSection($item);
+        }
+        return $sections;
+    }
+
+    // récupération du titre et de l'id de toutes les sections (menu + choix dans formulaires) en classant par nom de section ascendant
+    public function getAllWithoutTheSectionDesc(): array
+    {
+        $sql="SELECT idtheSection, theSectionName FROM TheSection ORDER BY theSectionName ASC";
         $recup = $this->db->query($sql);
         if(!$recup->rowCount()){
             return [];
